716fd375ee36f363e435b0e8aa317fcefc692f63
polli2203 <146850815+polli2203@users.noreply.github.com>
2024-07-04 14:54:12 -0300

    yfyuhjv

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ function showDescription(area) {
     let description = '';
 
     switch(area) {
+        case 'zootecnia':
+            description = 'Zootecnia: é a ciência que se dedica ao estudo da criação e produção de animais. O curso abrange temas como nutrição, manejo, genética e reprodução animal. O profissional estará  para atuar na gestão de fazendas, granjas e agroindústrias, visando a maximização da produção e a qualidade dos produtos de origem animal.';
+            break;
+            case 'engenharia florestal':
+            description = 'Este curso é focado na gestão sustentável de recursos naturais, especialmente florestas. Inclui estudo de silvicultura, manejo ambiental e conservação. Forma profissionais aptos a trabalhar na preservação e exploração responsável de recursos florestais, além de planejar e executar projetos de reflorestamento.';
+            break;
         case 'medicina':
             description = 'Medicina: Profissionais da saúde são essenciais para garantir o bem-estar das comunidades rurais.';
             break;
             case 'agronomia':
-                description = 'O curso de Agronomia é voltado para o estudo e desenvolvimento de técnicas e práticas agrícolas. Aborda temas como cultivo de plantas, manejo do solo, produção animal e gestão rural. O curso fornece base teórica e prática, habilitando o profissional a gerenciar propriedades agrícolas, desenvolver projetos de produção e aplicar inovações tecnológicas no campo.';
+                description = 'Agronomia: é voltado para o estudo e desenvolvimento de técnicas e práticas agrícolas. Aborda temas como cultivo de plantas, manejo do solo, produção animal e gestão rural. O curso fornece base teórica e prática, habilitando o profissional a gerenciar propriedades agrícolas, desenvolver projetos de produção e aplicar inovações tecnológicas no campo.';
                 break;
         case 'engenhariaAgricola':
             description = 'Engenharia Agrícola: Engenharia aplicada ao campo para otimizar a produção e a tecnologia agrícola.';
